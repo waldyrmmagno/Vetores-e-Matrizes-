e0850f100e6618ea7faec709e5f200b7e44674b3
waldyrmmagno <waldyrmmagno@gmail.com>
2025-03-19 11:10:18 -0300

    aula de vetores e matrizes

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] matriz = {
+                {1, 2, 3},
+                {4, 5, 6}
+        };
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.println(matriz[i][j]);
+            }
+        }
+    }
+}
+
+
+
+
